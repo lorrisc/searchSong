486718c07a80c03e8a777396ee13e879e3fe14c3
noix2kgouu <l.crappier@gmail.com>
2022-07-06 01:57:31 +0200

    comments

--- results/titles_artists.js
+++ results/titles_artists.js
@@ -1,9 +1,16 @@
+//*Switch section (button navbar)*/
+
+//*Navbar button*/
 const navTitlesButton = document.querySelector(".navbar p:nth-child(1)");
 const navArtistsButton = document.querySelector(".navbar p:nth-child(2)");
 
+//*container*/
 const contenairTitle = document.querySelector(".cardTitleContenair");
 const contenairArtist = document.querySelector(".cardArtistsContenair");
 
+
+
+//*title click*/
 navTitlesButton.addEventListener("click", () => {
     navTitlesButton.classList.add("activeNavbar");
     navArtistsButton.classList.remove("activeNavbar");
@@ -12,6 +19,7 @@ navTitlesButton.addEventListener("click", () => {
     contenairArtist.classList.remove("activeContenair");
 });
 
+//*artists click*/
 navArtistsButton.addEventListener("click", () => {
     navArtistsButton.classList.add("activeNavbar");
     navTitlesButton.classList.remove("activeNavbar");
